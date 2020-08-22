c4282064d2232248d75582debbbcbc5bf8d33ce7
Giovanna Albuquerque <ghb.albuquerque@gmail.com>
2020-08-22 14:41:44 -0300

    navbar solution

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,17 @@
+$(window).scroll(function() {
+    if ($(window).scrollTop() > $(".navbar").height()) {
+        $(".navbar").addClass('bg-white')
+        $(".navbar-link").addClass("dark")
+        $(".navbar-logo").addClass('logodark')
+        $(".navbar .material-icons").addClass('burguer-black')
+    } else {
+        $(".navbar").removeClass('bg-white')
+        $(".navbar-link").removeClass("dark")
+        $(".navbar-logo").removeClass('logodark')
+        $(".navbar .material-icons").removeClass('burguer-black')
+    }
+});
+
 
 $(".navbar-burguer").on('click', function () {
     $(".burguer-list").toggle("slow")
